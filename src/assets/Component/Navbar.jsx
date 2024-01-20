eb1452bd949c3ebb86ddd21756ef60cbfd14404d
Sumit shriwastava <sumit87478@gmail.com>
2024-01-20 16:27:02 +0530

    css changed

---
+++ src/assets/Component/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import  "./Navbar.css";
+
+const Navbar = () => {
+
+return (
+    <div>
+       <div className="nav"> 
+              <img src="https://layerly.io/wp-content/uploads/2022/04/Layerly-Logo-03.svg" width="10%"/>
+              <ul className="list">
+                <li>pricing</li>
+                <li>Add-ons</li>
+                <li>FAQs</li>
+                <li>Company</li>
+              </ul>
+       </div>
+    </div>
+)}
+
+export default Navbar
