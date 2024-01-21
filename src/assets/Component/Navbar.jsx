6c444018fe8e591f1eb90d994125393ecb510a29
Sumit shriwastava <sumit87478@gmail.com>
2024-01-21 15:18:11 +0530

    push

--- src/assets/Component/Navbar.jsx
+++ src/assets/Component/Navbar.jsx
@@ -6,17 +6,18 @@ const Navbar = () => {
 return (
     <div>
        <div className="navbar">
-        <div className="div-img">
+        <div className="div-img d2">
             <img className="image" src="https://layerly.io/wp-content/uploads/2022/04/Layerly-Logo-03.svg" alt="" />
         </div>
-        <div  className="List">
+        <div  className="List d2">
             <ul >
                 <li>Pricing</li>
                 <li>Add-ons</li>
-                <li>FAQS</li>
+                <li>FAQs</li>
+                <li>Company</li>
             </ul>
         </div>
-        <div className="div-btn">
+        <div className="div-btn d2">
             <button className="btn">Start Here</button>
         </div>
        </div>
